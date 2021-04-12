87f601014e5abd7da5cae7d2bfe27d10334de821
Parrot Live user <ajayakumbhar1234@gmail.com>
2021-04-12 18:33:53 +0000

    updated file.

--- src/components/Upload/UploadFile.js
+++ src/components/Upload/UploadFile.js
@@ -1,17 +1,54 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { VscFile } from "react-icons/vsc";
 
 const UploadFile = ({ fileType }) => {
 
-    const [file, setFile] = useState(null);
+    const [file, setFile] = useState('');
+    const [loading, setLoading] = useState(false);
 
-    const onFileChange = (event) => {
+    const onFileChange = async (event) => {
+        setLoading(true)
+        if (!event.target.files[0]) {
+            console.log('file not found');
+            return
+        }
         setFile(event.target.files[0]);
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const responseUplaod = await axios.post(
+                'http://localhost:8080/document',
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            )
+            const url = responseUplaod.data.url;
+
+            const responseConvert = await axios.post('http://localhost:8080/document/convert', {
+                from: 'office',
+                to: 'pdf',
+                url
+            })
+            const convertedUrl = responseConvert.data.url;
+
+            setLoading(false);
+            alert("suceessfully conveted")
+        } catch (error) {
+            setLoading(false)
+            setFile('')
+            console.log(error);
+        }
     }
 
 
     return (
         <div className="flex flex-col items-center">
+            {loading ? "loading..." : ''}
             <label for="file" className="px-8 py-4 rounded  bg-gray-50 cursor-pointer  flex items-center">
                 <input type="file" id="file" name="file" accept={fileType} className="hidden" onChange={onFileChange} />
                 <span className="m-1 text-2xl text-gray-600"> <VscFile /> </span>
