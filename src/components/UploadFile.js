dda140c26c22e9d918a49e5a6ce1bef53f9e3167
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-11 10:27:23 +0530

    upload file component added with code

---
+++ src/components/UploadFile.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+const UploadFile = () => {
+
+    const [file, setFile] = useState(null);
+
+    const onFileChange = (event)=>{
+        setFile(event.target.files[0]);
+    }
+
+    const uplaodFile = ()=>{
+        const formData = new FormData();
+        formData.append("file",file)
+        
+    }
+
+    return (
+        <div>
+            <input type="file" onChange={onFileChange}/>
+            <button onClick={uplaodFile}> Uplaod file </button>
+        </div>
+    )
+}
+
+export default UploadFile
