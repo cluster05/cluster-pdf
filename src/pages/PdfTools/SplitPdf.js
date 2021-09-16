e3673cad61984db0d4b3fc1cbd3cbf2eb18eec0f
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-17 04:01:01 +0530

    split-pdf ui added.

--- src/pages/PdfTools/SplitPdf.js
+++ src/pages/PdfTools/SplitPdf.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Upload from '../../components/Upload/Upload';
 
 const SplitPdf = () => {
     return (
         <div>
-            split work
+            <Upload
+                title={"Split PDF"}
+                subtitle={"Split your PDF online."}
+                fileType={".pdf"}
+                color={"red"}
+            />
         </div>
     )
 }
