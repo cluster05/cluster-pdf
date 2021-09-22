6988cbdba3ec235e3e207c69a59bd1578aff435c
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-21 23:51:54 -0400

    View pdf Look established

--- src/pages/ViewPdf/ViewPdf.js
+++ src/pages/ViewPdf/ViewPdf.js
@@ -24,11 +24,10 @@ const ViewPdf = () => {
     }
 
     return (
-      <div className="">
-         
-        <div className=" flex justify-center h-full ">
+      <div className="grid grid-cols-7">
+        <div className="col-span-5 w-full flex h-screen overflow-y-scroll justify-center">
           <Document
-            className=" max-w-sm md:max-w-md lg:max-w-6xl flex flex-wrap justify-center"
+            className=" m-10  flex flex-wrap"
             file={`http://localhost:8080/document/${pdfId}`}
             onLoadSuccess={onDocumentLoadSuccess}
             options={options}
@@ -38,7 +37,8 @@ const ViewPdf = () => {
                 new Array(numPages),
                 (el, index) => (
                   <Page
-                  className="border-2 m-5 rounded shadow-xl"
+                 
+                  className="m-5 border shadow-md rounded"
                   key={`page_${index + 1}`}
                   pageNumber={index + 1}
                   />
@@ -47,7 +47,26 @@ const ViewPdf = () => {
                 }
           </Document>
         </div>
-        <div className=""> Hello </div>
+        <div className="col-span-2"> 
+              <div className="bg-gray-100 h-full ">
+                <div className="px-5 py-10">
+                <h2 className="text-5xl font-bold"> ClusterPdf </h2>
+                <p className="text-gray-800 font-medium text-xl my-2"> Conveted Word Document into PDF </p>
+               
+                <div className="border border-gray-800 mb-10"></div>
+                <div className="flex items-center">
+                  <p className="my-3 text-2xl"> pdf name goes here.pdf </p>
+                  <button className="ml-5 px-2 py-1 capitalize rounded bg-green-500 text-white">change name</button>
+                </div>
+                  <div className="mt-10"></div>
+                  <a  
+                    className="px-8 py-3 text-2xl font-bold capitalize rounded bg-blue-600 text-white" 
+                    download="filename"
+                    href={`http://localhost:8080/document/${pdfId}`}> download PDF </a>
+                </div>
+              </div>
+
+         </div>
        
       </div>
 
