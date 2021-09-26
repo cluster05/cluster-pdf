5634c4610dd7ef3b0c83327da32ca48a7d7e9f4f
cluster05 <ajayakumbhar1234@gmail.com>
2021-09-26 10:51:59 +0530

    PDF Print Direct Added

--- src/pages/ViewPdf/PDFPrinter.js
+++ src/pages/ViewPdf/PDFPrinter.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { FaPrint } from 'react-icons/fa';
 
-const PDFPrinter = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const PDFPrinter = ({ file }) => {
 
-export default PDFPrinter
+  const print = () => {
+    const pdfFrame = document.createElement('iframe');
+    pdfFrame.style.visibility = 'hidden';
+    pdfFrame.src = file;
+
+    document.body.appendChild(pdfFrame);
+
+    pdfFrame.contentWindow.focus();
+    pdfFrame.contentWindow.print();
+  };
+  return (
+      <FaPrint onClick={print} title={"download"}/>          
+  );
+};
+
+export default PDFPrinter;
