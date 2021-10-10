536ccdea2697a5e2e18c0f34233ba835883e6759
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-10 12:27:53 +0530

    Backend PORT changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 const AWS_BUCKET_URL="https://clusterpdf-bucket.s3.ap-south-1.amazonaws.com"
 
-const BACKEND_BASE_URL ="http://localhost:8080"
+const BACKEND_BASE_URL ="http://localhost:8000"
 
 export {
     AWS_BUCKET_URL,
