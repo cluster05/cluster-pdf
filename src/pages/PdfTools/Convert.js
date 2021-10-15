63bc7809cdf8938edde8be3142e4845001795939
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-15 17:48:51 +0530

    page under construction route added

--- src/pages/PdfTools/Convert.js
+++ src/pages/PdfTools/Convert.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import { getDirection } from '../../tools/ConvertDirection';
-import UploadConvert from '../../components/UploadConvert/UploadConvert';
-import { useHistory } from 'react-router';
+import React, { useEffect } from "react";
+import { getDirection } from "../../tools/ConvertDirection";
+import UploadConvert from "../../components/UploadConvert/UploadConvert";
+import { useHistory } from "react-router";
 
 const Convert = () => {
+  const history = useHistory();
+  let routename = history.location.pathname;
 
-    
-    const history = useHistory();
-    let routename = history.location.pathname;
-    
-    routename =  routename.substr(1,routename.length);
+  routename = routename.substr(1, routename.length);
 
-    const opration = routename.split("-").join("");
-    
-    const metadata = getDirection[opration];
-    return (
-        <div>
-            <UploadConvert 
-                apiMetadeta={metadata.apiMetaData}
-                componentMetadata={metadata.componentMetadata}
-            />
-        </div>
-    )
-}
+  const opration = routename.split("-").join("");
 
-export default Convert
+  const metadata = getDirection[opration];
+
+  useEffect(() => {
+    if (
+      metadata.apiMetaData.APIRequestBody.from == "pdf" ||
+      metadata.apiMetaData.APIRequestBody.to == "office"
+    ) {
+      history.push("/under-process");
+      return;
+    }
+  }, []);
+
+  return (
+    <div>
+      <UploadConvert
+        apiMetadeta={metadata.apiMetaData}
+        componentMetadata={metadata.componentMetadata}
+      />
+    </div>
+  );
+};
+
+export default Convert;
