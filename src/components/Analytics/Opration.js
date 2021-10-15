30062626f3b733695c07fe89292d7528bc865f9c
cluster05 <ajayakumbhar1234@gmail.com>
2021-10-15 17:15:26 +0530

    Operation title persist

--- src/components/Analytics/Opration.js
+++ src/components/Analytics/Opration.js
@@ -3,17 +3,20 @@ import AnalyticsCard from "./AnalyticsCard";
 
 const Opration = ({ opration }) => {
   return (
-    <div className="flex items-center flex-wrap">
-      {opration.map((ele) => {
-        return (
-          <AnalyticsCard
-            key={ele.opration}
-            title={ele.opration}
-            count={ele.count}
-          />
-        );
-      })}
-    </div>
+    <React.Fragment>
+      <h1 className="m-4 font-black text-6xl">Opration</h1>
+      <div className="flex items-center flex-wrap">
+        {opration.map((ele) => {
+          return (
+            <AnalyticsCard
+              key={ele.opration}
+              title={ele.opration}
+              count={ele.count}
+            />
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 };
 
